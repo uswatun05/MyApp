0d4fc6deaae048eaf5553631e56ab222bb3c4721
unknown <uswtun@gmail.com>
2025-04-27 12:54:50 +0700

    15

--- app/_layout.js
+++ app/_layout.js
@@ -3,8 +3,8 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
-      <Stack.Screen name="RecipeDetail" options={{ title: 'Detail Resep' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="detail/[id]" options={{ headerShown: false  }} />
     </Stack>
   );
 }
